956f731463ee9011454357907b051361ed03ee23
Aurelle Martin <martinaurelle.ma@gmail.com>
2018-04-10 11:25:08 +0200

    martin

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<div class="navbar">
+    <a class="navbar-brand" href="#">Upload ton file</a>
+    <ul class="nav navbar-nav">
+        <li class="active">
+            <a href="index.php">Check tes images</a>
+        </li>
+        <li>
+            <a href="admin.php">Upload</a>
+        </li>
+    </ul>
+</div>
+</html>
